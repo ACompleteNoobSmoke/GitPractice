9c25e265ed627bd6699b067f7afc9d28ef1548c0
Omoruyi Omofonmwan <OmoruyiOmofonmwan@yahoo.com>
2020-02-21 12:21:24 -0600

    Made some changes

--- newFile.java
+++ newFile.java
@@ -3,5 +3,7 @@ public class newFile{
 	public static void main(String[] args){
 		String name = "Omoruyi Omofonmwan";
 		System.out.println("I really think I am starting to understand all of this dude " + name);
+
+		System.out.println("Once I comeback I will get this done, good job though");
 	}
 }
