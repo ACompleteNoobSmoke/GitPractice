65bfb33ac39150acfc213f0471587bf28b3c4e13
Omoruyi Omofonmwan <OmoruyiOmofonmwan@yahoo.com>
2020-02-21 11:53:19 -0600

    Just testing this out again. I think I understand

--- S_Runner.java
+++ S_Runner.java
@@ -2,7 +2,7 @@ public class S_Runner {
 
     public static void main(String[] args) {
         
-        System.out.println("This is a new branch test");
+        System.out.println("I will be so good as a programmer that I will be the one getting sought out");
 
     }
 }
